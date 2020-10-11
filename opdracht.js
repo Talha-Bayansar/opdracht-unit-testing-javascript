d330d27342164b2291dab25a64eaaa1753e64563
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-11 21:52:27 +0200

    added tests for the first 4 situations

--- opdracht.js
+++ opdracht.js
@@ -1,5 +1,9 @@
-const functions = {
+const opdracht = {
+    stock: 0,
+    buyStock: (amount) => {opdracht.stock += amount; return opdracht.stock},
+    sellStock: (amount) => {opdracht.stock -= amount; return opdracht.stock},
+    promotion: () => {opdracht.stock >= 20 ? opdracht.stock += 5 : opdracht.stock = opdracht.stock; return opdracht.stock},
 
 };
 
-module.exports = functions;
+module.exports = opdracht;
--- opdracht.test.js
+++ opdracht.test.js
@@ -1,5 +1,21 @@
 const { expect } = require("@jest/globals");
-const functions = require("./functions");
+const opdracht = require("./opdracht");
+
+test("starting situation", () => {
+    expect().toBe();
+});
+
+test("buying stocks", () => {
+    expect(opdracht.buyStock(10)).toBe(10);
+});
+
+test("selling stocks", () => {
+    expect(opdracht.sellStock(10)).toBe(0);
+});
+
+test("promotion", () => {
+    expect(opdracht.promotion()).toBe(0);
+});
 
 test("", () => {
     expect().toBe();
