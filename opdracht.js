6399785484f0d86efbe51d30c20a7434ceaeb0b6
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-11 23:20:37 +0200

    added coin functionality

--- opdracht.js
+++ opdracht.js
@@ -1,9 +1,46 @@
 const opdracht = {
-    stock: 0,
-    buyStock: (amount) => {opdracht.stock += amount; return opdracht.stock},
-    sellStock: (amount) => {opdracht.stock -= amount; return opdracht.stock},
-    promotion: () => {opdracht.stock >= 20 ? opdracht.stock += 5 : opdracht.stock = opdracht.stock; return opdracht.stock},
-
+    stock: 10,
+    coins: 10,
+    buyStock: (amount) => {
+        if(opdracht.coins < amount){
+            return false;
+        }
+        opdracht.stock += amount;
+        opdracht.coins -= amount;
+        return opdracht.stock
+    },
+    sellStock: (amount) => {
+        if(amount > opdracht.stock){
+            return false;
+        }else{
+            opdracht.stock -= amount;
+            opdracht.coins += amount;
+            return opdracht.stock;
+        }
+    },
+    promotion: () => {
+        if(opdracht.stock >= 10){
+            opdracht.stock += 5;
+            return opdracht.stock;
+        }else{
+            return false;
+        }
+    },
+    renewStock: () => opdracht.stock == 0 && opdracht.buyStock(10),
+    addCoins: (amount) => {
+        if(amount < 0){
+            return false;
+        }
+        opdracht.coins += amount;
+        return opdracht.coins
+    },
+    subtractCoins: (amount) => {
+        if(amount < 0){
+            return false;
+        }
+        opdracht.coins -= amount;
+        return opdracht.coins
+    },
 };
 
 module.exports = opdracht;
--- opdracht.test.js
+++ opdracht.test.js
@@ -2,21 +2,60 @@ const { expect } = require("@jest/globals");
 const opdracht = require("./opdracht");
 
 test("starting situation", () => {
-    expect().toBe();
+    expect(opdracht.stock).toBe(10);
+    expect(opdracht.coins).toBe(10);
+});
+
+test("buying stocks", () => {
+    expect(opdracht.buyStock(10)).toBe(20);
+    expect(opdracht.coins).toBe(0);
+});
+
+test("selling stocks", () => {
+    expect(opdracht.sellStock(20)).toBe(0);
+    expect(opdracht.coins).toBe(20);
+    expect(opdracht.stock).toBe(0);
+});
+
+test("promotion not possible", () => {
+    expect(opdracht.promotion()).toBe(false);
 });
 
 test("buying stocks", () => {
     expect(opdracht.buyStock(10)).toBe(10);
+    expect(opdracht.coins).toBe(10);
 });
 
-test("selling stocks", () => {
-    expect(opdracht.sellStock(10)).toBe(0);
+test("more orders then stock", () => {
+    expect(opdracht.sellStock(50)).toBe(false);
+});
+
+test("renewing stocks when not empty", () => {
+    expect(opdracht.renewStock()).toBe(false);
+});
+
+test("not enough coins", () => {
+    expect(opdracht.buyStock(50)).toBe(false);
 });
 
 test("promotion", () => {
-    expect(opdracht.promotion()).toBe(0);
+    expect(opdracht.promotion()).toBe(15);
 });
 
-test("", () => {
-    expect().toBe();
+test("adding positive number of coins", () => {
+    expect(opdracht.addCoins(10)).toBe(20);
+});
+
+test("adding negative number of coins is not possible", () => {
+    expect(opdracht.addCoins(-10)).toBe(false);
+    expect(opdracht.coins).toBe(20);
+});
+
+test("subtract coins", () => {
+    expect(opdracht.subtractCoins(5)).toBe(15);
+});
+
+test("subtracting negative number of coins is not possible", () => {
+    expect(opdracht.addCoins(-10)).toBe(false);
+    expect(opdracht.coins).toBe(15);
 });
